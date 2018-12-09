c6cf7ac37cf006bccf88d10814572d2adecd581c
olaadediji <34110255+olaadediji@users.noreply.github.com>
2018-12-09 23:10:15 +0000

    Add files via upload

---
+++ CW_OlaAdediji/CW_OlaAdediji/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CW_OlaAdediji
+//
+//  Created by Ola on 09/12/2018.
+//  Copyright © 2018 Ola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
