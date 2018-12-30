6cf115ed439b5c67960f626870430b0f81632355
olaadediji <34110255+olaadediji@users.noreply.github.com>
2018-12-30 10:47:32 +0000

    Add files via upload
    Commit1

--- CW_OlaAdediji/CW_OlaAdediji/ViewController.swift
+++ CW_OlaAdediji/CW_OlaAdediji/ViewController.swift
@@ -9,12 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+ //Changes
+    
+    @IBOutlet weak var cloud: UIImageView!
+    @IBOutlet weak var cloud2: UIImageView!
+    @IBOutlet weak var Road: UIImageView!
+    @IBOutlet weak var tree: UIImageView!
+    
+    //@IBOutlet weak var cloud1: UIImageView!
+    //@IBOutlet weak var cloud2: UIImageView!
+    
+    override func viewDidLoad(){
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+           //Animation for road
+        var imageArray: [UIImage]
+        imageArray = [UIImage(named: "road1.png")!,
+                      UIImage(named: "road2.png")!,
+                      UIImage(named: "road3.png")!,
+                      UIImage(named: "road4.png")!,
+                      UIImage(named: "road5.png")!,
+                      UIImage(named: "road5.png")!,
+                      UIImage(named: "road6.png")!,
+                      UIImage(named: "road7.png")!,
+                      UIImage(named: "road8.png")!,
+                      UIImage(named: "road9.png")!,
+                      UIImage(named: "road10.png")!,
+                      UIImage(named: "road11.png")!,
+                      UIImage(named: "road12.png")!,
+                      UIImage(named: "road13.png")!,
+                      UIImage(named: "road14.png")!,
+                      UIImage(named: "road15.png")!,
+                      UIImage(named: "road16.png")!,
+                      UIImage(named: "road17.png")!,
+                      UIImage(named: "road18.png")!,
+                      UIImage(named: "road19.png")!]
+                     //UIImage(named: "tree1.png")!]
+        
+        Road.image = UIImage.animatedImage(with: imageArray, duration: 0.4)
+        //tree.image = UIImage.animatedImage(with: imageArray, duration: 0.6)
+        
+            //imageArray = [UIImage(named: "Tree1.png")]
 
+        
+    
+    
+        
+        UIView.animate(withDuration: 2.5, delay: 0, options:
+            [UIView.AnimationOptions.repeat, .curveLinear], animations:
+            {
+                self.cloud.center.x -= self.view.bounds.width
+                self.cloud2.center.x -= self.view.bounds.width
+                self.tree.center.x -= self.view.bounds.width
+
+        }, completion: nil
+        )
+        
+        
+    }
+    
+    
+    
+
+    
+    
+    
 
 }
 
