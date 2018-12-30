c81e204fa0a1ad93344578a4ae8056c7d1f5b5d3
olaadediji <34110255+olaadediji@users.noreply.github.com>
2018-12-30 10:58:41 +0000

    Add files via upload

---
+++ CW_OlaAdediji/CW_OlaAdediji/CW_OlaAdediji/UI.swift
@@ -0,0 +1,21 @@
+//
+//  UI.swift
+//  CW_OlaAdedijiTests
+//
+//  Created by Ola on 18/12/2018.
+//  Copyright © 2018 Ola. All rights reserved.
+//
+
+import UIKit
+
+class UI: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
